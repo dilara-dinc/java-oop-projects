e09faef181f482412c628dfc15374f0fc37f6aa4
Dilara Dinç <dilara.dinc.d@gmail.com>
2026-04-02 01:44:09 +0300

    Add Book model class

---
+++ library-management-system/src/model/Book.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Book ID: " + id +
+               ", Title: " + title +
+               ", Author: " + author +
+               ", Borrowed: " + isBorrowed;
+    }
+}
