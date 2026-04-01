22515b11f9a0c18095d21dc99af9df37261e3a11
Dilara Dinç <dilara.dinc.d@gmail.com>
2026-04-02 01:31:35 +0300

    Add Account class

---
+++ atm-system/src/model/Account.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Account {
+
+    private String accountHolder;
+    private double balance;
+
+    public Account(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposit successful.");
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful.");
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
+}
